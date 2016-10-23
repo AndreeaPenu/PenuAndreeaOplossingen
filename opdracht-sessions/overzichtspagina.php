d68da26f37d5326e1a6f4d717215beb4ee5b0ce4
Andreea Penu <Andreea Penu>
2016-10-23 09:57:13 +0200

    post wijzig checken en id in session steken?

--- opdracht-sessions/overzichtspagina.php
+++ opdracht-sessions/overzichtspagina.php
@@ -6,6 +6,12 @@
 			$_SESSION['gemeente'] = $_POST['gemeente'];
 			$_SESSION['postcode'] = $_POST['postcode'];	
 	} 
+
+	if (isset($_POST['wijzig'])){
+		$id = $_GET['id'];
+		$_SESSION[$id] = $_POST[$id];
+	}
+	
 ?>
 <!DOCTYPE html>
 <html>
@@ -14,12 +20,12 @@
 <body>
 		<h2>Registratiegegevens</h2>
 		<ul>
-			<li><?php echo "e-mail: " . $_SESSION['email'] ?> <a name="wijzig" href="registratiepagina.php" putFocusOn="email"> Wijzig </a></li>
-			<li><?php echo "nickname: " . $_SESSION['nickname'] ?> <a name="wijzig" href="registratiepagina.php"> Wijzig </a></li>
-			<li><?php echo "straat: " . $_SESSION['straat'] ?> <a name="wijzig" href="adrespagina.php"> Wijzig </a></li>
-			<li><?php echo "nummer: " . $_SESSION['nummer'] ?> <a name="wijzig" href="adrespagina.php"> Wijzig </a></li>
-			<li><?php echo "gemeente: " . $_SESSION['gemeente'] ?> <a name="wijzig" href="adrespagina.php"> Wijzig </a></li>
-			<li><?php echo "postcode: " . $_SESSION['postcode'] ?> <a name="wijzig" href="adrespagina.php"> Wijzig </a></li>
+			<li><?php echo "e-mail: " . $_SESSION['email'] ?> <a name="wijzig" id="email" action="registratiepagina.php" method="post"> Wijzig </a></li>
+			<li><?php echo "nickname: " . $_SESSION['nickname'] ?> <a name="wijzig" id="nickname" href="registratiepagina.php"> Wijzig </a></li>
+			<li><?php echo "straat: " . $_SESSION['straat'] ?> <a name="wijzig" id="straat" href="adrespagina.php"> Wijzig </a></li>
+			<li><?php echo "nummer: " . $_SESSION['nummer'] ?> <a name="wijzig" id="nummer" href="adrespagina.php"> Wijzig </a></li>
+			<li><?php echo "gemeente: " . $_SESSION['gemeente'] ?> <a name="wijzig" id="gemeente" href="adrespagina.php"> Wijzig </a></li>
+			<li><?php echo "postcode: " . $_SESSION['postcode'] ?> <a name="wijzig" id="postcode" href="adrespagina.php"> Wijzig </a></li>
 		<ul>
 </body>
 </html>			
