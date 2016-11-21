32053a72657d148ee7f03694699b24bcc6709e87
Andreea Penu <Andreea Penu>
2016-11-21 19:29:55 +0100

    opdracht classes-extends Zebra klasse

--- opdracht-classes-extends/application.php
+++ opdracht-classes-extends/application.php
@@ -7,6 +7,9 @@
 
 	$lion1 = new Lion('Simba','Congo lion','roar');
 	$lion2 = new Lion('Scar','Kenia lion','roar');
+
+	$zebra1 = new Zebra('Zeke','Quagga');
+	$zebra2 = new Zebra('Zena','Selous');
 ?>
 
 
@@ -25,8 +28,20 @@
 	<p><?= $animal3->$name ?>is van het geslacht <?= $animal3->$gender ?> en heeft momenteel levenspunten <?= $animal3->$health ?>(special move: <?= $animal3->$doSpecialMove() ?>)</p>
 
 
+
+
+
 	<p>De speciale move van <?= $lion1->$name ?> (soort:<?= $lion1->$species ?>):<?= $lion1->$doSpecialMove() ?></p>
 
 	<p>De speciale move van <?= $lion2->$name ?> (soort:<?= $lion2->$species ?>):<?= $lion2->$doSpecialMove() ?></p>
+
+
+	<p>De speciale move van <?= $zebra1->$name ?> (soort:<?= $zebra1->$species ?>):<?= $zebra1->$doSpecialMove() ?></p>
+
+	<p>De speciale move van <?= $zebra2->$name ?> (soort:<?= $zebra2->$species ?>):<?= $zebra2->$doSpecialMove() ?></p>
+
+
+
+
 </body>
 </html>
