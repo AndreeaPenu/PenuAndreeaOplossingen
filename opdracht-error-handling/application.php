aeae56024832c8218e88f63659fd6ddf2d2bb4b2
Andreea Penu <Andreea Penu>
2016-11-21 21:44:11 +0100

    error-handling html

---
+++ opdracht-error-handling/application.php
@@ -0,0 +1,23 @@
+<?php
+
+
+
+
+
+?>
+
+
+<!doctype html>
+<html>
+<head>
+    <meta charset="utf-8">
+    
+</head>
+
+<body>
+	<h2>Geef uw kortingscode op</h2>
+	<label for="kortingscode">Kortingscode</label>
+	<input id="kortingscode" name="kortingscode" type="text">
+	<input type="submit"></input>
+</body>
+</html>
