60cd8ec0762ed68cc8cc8d9e956959e39b6d5386
Andreea Penu <Andreea Penu>
2016-11-27 17:42:38 +0100

    gegevens correct + table begin

--- opdracht-CRUD-query/opdracht-crud-query.php
+++ opdracht-CRUD-query/opdracht-crud-query.php
@@ -19,11 +19,22 @@
 
 
 
-	$statement 	= 	$db->prepare('SELECT bieren.brouwernr
-									FROM bieren');
+	$queryString = 	('	SELECT *
+						FROM bieren 
+						INNER JOIN brouwers
+						ON bieren.brouwernr = brouwers.brouwernr 
+						WHERE bieren.naam LIKE "du%" AND brouwers.brnaam LIKE "%a%"
+					');
 
 	$statement = $db->prepare($queryString);
+
 	$statement->execute();
+	$fetchAssoc 	=	array(); 
+
+	while ( $row = $statement->fetch(PDO::FETCH_ASSOC) )
+		{
+			$fetchAssoc[]	=	$row;
+		}
 
 ?>
 
@@ -42,6 +53,41 @@
 		<h1>Voorbeeld van database connectie - PDO</h1>	
 
 		<p><?php echo $messageContainer ?></p>
+
+
+
+		<ul>
+			<?php foreach ($fetchAssoc as $row): ?>
+				<li><?php echo $row['naam'] ?>: <?php echo $row['alcohol'] ?> promille</li>
+			<?php endforeach ?>
+		</ul>
+
+
+
+
+		<table>
+			<thead>
+				<tr>
+					<th>Biernr PK</th>
+					<th>naam</th>
+					<th>brouwernr</th>
+					<th>soortnr</th>
+					<th>alcohol</th>
+					<th>brnaam</th>
+					<th>adres</th>
+					<th>postcode</th>
+					<th>gemeente</th>
+					<th>omzet</th>
+				</tr>
+			</thead>
+			<tfoot>
+			</tfoot>
+			<tbody>
+
+			</tbody>
+			
+
+		</table>
 
 	
 			
