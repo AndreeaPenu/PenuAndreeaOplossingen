8bf91dd0ce1f6d39ffb74ef9b7ab051dd0c13843
Andreea Penu <Andreea Penu>
2016-11-27 16:59:09 +0100

    verplaatsing code van index naar builder

--- opdracht-classes-portfolio/classes/HTMLBuilder.php
+++ opdracht-classes-portfolio/classes/HTMLBuilder.php
@@ -3,6 +3,27 @@
 
 
 
+
+		public function __construct($header,$body,$footer){
+			$this->header = $header;
+			$this->body = $body;
+			$this->footer = $footer;
+		}
+
+		public function findFiles($dir,$file){
+
+		}
+
+		private function buildJsLinks(){
+			findFiles('/js/script.js','js');
+		}
+
+		private function buildCssLinks(){
+			findFiles('/css/global.css','css');
+		}
+
+
+
 		public function buildHeader(){
 			include 'html/header-partial.php';
 			include 'css/global.css';
--- opdracht-classes-portfolio/index.php
+++ opdracht-classes-portfolio/index.php
@@ -1,33 +1,8 @@
 <?php
 	require_once '/classes/HTMLBuilder.php';
 
-	$builder = new HTMLBuilder();
-
-
-
-	$buildHeader = $builder->$buildHeader();
-	$buildBody = $builder->$buildBody();
-	$buildFooter = $builder->$buildFooter();
-
-	$buildHead;
-	$buildBody;
-	$buildFooter;
-
-	public function __construct($header,$body,$footer){
-
-	}
-
-	public function findFiles($dir,$file){
-
-	}
-
-	private function buildJsLinks(){
-		findFiles('/js/script.js','js');
-	}
-
-	private function buildCssLinks(){
-		findFiles('/css/global.css','css');
-	}
-
+	$builder = new HTMLBuilder($header,$body,$footer);
 
 ?>
+
+
