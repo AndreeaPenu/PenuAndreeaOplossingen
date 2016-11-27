fb4b3e5f8c8a8b19f42a31c45f9a2e829f3acc48
Andreea Penu <Andreea Penu>
2016-11-27 16:32:09 +0100

    error-handling  to do: createMessage en showMessage

--- opdracht-error-handling/application.php
+++ opdracht-error-handling/application.php
@@ -1,6 +1,10 @@
 <?php
 
+
+session_start();
+
 $kortingscode = '';
+$isValid = false;
 
 try
 	{
@@ -12,6 +16,11 @@ try
 				}
 				else
 				{
+					if(strlen($_POST['kortingscode']) !=8){
+						throw new Exception('VALIDATION-CODE-LENGTH');
+					}else{
+						$isValid=true;
+					}
 					
 				}		
 		}
@@ -19,12 +28,12 @@ try
 	catch ( Exception $e )
 	{
 
-		$messageCode; //spreekt e class aan en vangt error
+		$messageCode = $e->getMessage(); //spreekt e class aan en vangt error
 		$message	=	array(); //boodschap en type ontvangen
 		$createMessage = false; //boodschap in klasse zetten of niet
 
-		$message['type']	=	'error';
-		$message['text']	=	$e->getMessage();
+		//$message['type']	=	'error';
+		//$message['text']	=	$e->getMessage();
 
 
 
@@ -32,8 +41,16 @@ try
 			case 'SUBMIT-ERROR': $message['type'] = 'error';
 								 $message['text'] = 'Er werd met het formulier geknoeid';
 								 break;
-			default:'default';
+			case 'VALIDATION-CODE-LENGTH': $message['type'] = 'error';
+										   $message['text'] = 'De kortingscode heeft niet de juiste lengte';
+										   $createMessage = true;
+										   break;
 
+		}
+
+
+		if($createMessage == true){
+			createMessage($message);
 		}
 
 		logToFile($message);	
@@ -44,12 +61,31 @@ try
 	function logToFile($message){
 		//[11:12:53 08/08/2015] - 127.0.0.1 - type:[error] Kortingscode is niet correct
 
-		$errorString	=	$date . ' ' . '-' . $message['type'] . " " . $message['text'] . "\n\r";
+		$date	=	'[' . date( 'H:i:s', time() ). ' ' . date("d/m/Y").']';
+
+		
+
+
+		$errorString	=	$date . ' - ' . $_SERVER['REMOTE_ADDR'] . ' - type:[' . $message['type'] . "] " . $message['text'] . "\n\r";
 
 		file_put_contents( 'log.txt', $errorString, FILE_APPEND );
 
 	}
 
+	function createMessage($message){
+
+
+	//	$type = $_SESSION[ 'message' ][ 'type' ];
+	//	$bericht = $_SESSION[ 'message' ][ 'message' ];
+
+
+
+	}
+
+
+	function showMessage(){
+
+	}
 
 
 ?>
@@ -66,16 +102,20 @@ try
 	<h2>Geef uw kortingscode op</h2>
 
 	
-		<?php if ( $message ): ?>
-		 	<div class="modal <?= $message[ 'type' ] ?>">
-		 		<?= $message[ 'text' ] ?>
-		 	</div>
-		<?php endif ?>
 
-	<form action="<?= $_SERVER['PHP_SELF'] ?>" method="POST">
-		<label for="kortingscode">Kortingscode</label>
-		<input id="kortingscode" name="kortingscode" type="text">
-		<input type="submit" name="submit"></input>
-	</form>
+	<!-- if $isValid == true weergeef <p> met Korting toegekend! en hide form -->
+
+	<?php if ( $isValid != true): ?>
+
+	
+
+		<form action="<?= $_SERVER['PHP_SELF'] ?>" method="POST">
+			<label for="kortingscode">Kortingscode</label>
+			<input id="kortingscode" name="kortingscode" type="text">
+			<input type="submit" name="submit"></input>
+		</form>
+	<?php else: ?>
+		<p>Korting toegekend!</p>
+	<?php endif?>
 </body>
 </html>
