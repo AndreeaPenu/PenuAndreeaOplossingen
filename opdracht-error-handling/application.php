3b7ed7d595cdcd10ed20c923b8c077ed78b18c0b
Andreea Penu <Andreea Penu>
2016-11-27 15:42:54 +0100

    try catch aangepast (enkel1)

--- opdracht-error-handling/application.php
+++ opdracht-error-handling/application.php
@@ -1,50 +1,52 @@
 <?php
 
 $kortingscode = '';
-$message	=	false;	
-
-$messageCode;
-$createMessage =false;
-
-
 
 try
 	{
 		if ( isset ( $_POST['submit'] ) )
 		{
-			try
-			{
 				if ( $_POST['kortingscode'] == '' )
 				{
 					throw new Exception( 'SUBMIT-ERROR' );
 				}
 				else
 				{
-					$kortingscode	=	$_POST['kortingscode'];
-				}
-			}
-			catch( Exception $e )
-			{
-
-
-
-				$message['type']	=	'error';
-				$message['text']	=	$e->getMessage();
-
-				$date	=	'[' . date( 'H:i:s', time() ).']';
-
-				$errorString	=	$date . ' ' . $message['text'] . "\n\r";
-
-				file_put_contents( 'log.txt', $errorString, FILE_APPEND );
-
-			}
-			
+					
+				}		
 		}
 	}
 	catch ( Exception $e )
 	{
+
+		$messageCode; //spreekt e class aan en vangt error
+		$message	=	array(); //boodschap en type ontvangen
+		$createMessage = false; //boodschap in klasse zetten of niet
+
 		$message['type']	=	'error';
 		$message['text']	=	$e->getMessage();
+
+
+
+		switch($messageCode){
+			case 'SUBMIT-ERROR': $message['type'] = 'error';
+								 $message['text'] = 'Er werd met het formulier geknoeid';
+								 break;
+			default:'default';
+
+		}
+
+		logToFile($message);	
+
+	}
+
+
+	function logToFile($message){
+		//[11:12:53 08/08/2015] - 127.0.0.1 - type:[error] Kortingscode is niet correct
+
+		$errorString	=	$date . ' ' . '-' . $message['type'] . " " . $message['text'] . "\n\r";
+
+		file_put_contents( 'log.txt', $errorString, FILE_APPEND );
 
 	}
 
@@ -63,6 +65,12 @@ try
 <body>
 	<h2>Geef uw kortingscode op</h2>
 
+	
+		<?php if ( $message ): ?>
+		 	<div class="modal <?= $message[ 'type' ] ?>">
+		 		<?= $message[ 'text' ] ?>
+		 	</div>
+		<?php endif ?>
 
 	<form action="<?= $_SERVER['PHP_SELF'] ?>" method="POST">
 		<label for="kortingscode">Kortingscode</label>
