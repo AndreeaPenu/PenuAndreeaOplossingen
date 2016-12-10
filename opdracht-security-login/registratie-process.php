c5295f268e8746bf51d15087bd1366d01bcb348c
Andreea Penu <Andreea Penu>
2016-12-10 19:57:46 +0100

    verderwerken security

--- opdracht-security-login/registratie-process.php
+++ opdracht-security-login/registratie-process.php
@@ -2,29 +2,57 @@
 
 	session_start();
 
+	$email = $_SESSION["email"];
+	$password = $_SESSION["password"];
+
+
 	$value="";
 
 	
+	if(isset($_POST["registreren"])){
+		try{
+			$db = new PDO('mysql:host=localhost;dbname=phpoefening029', 'root', 'root', array (PDO::ATTR_ERRMODE => PDO::ERRMODE_EXCEPTION)); // Connectie maken
+		
+			$queryUser = 'SELECT * FROM users
+						WHERE username=:username
+						AND password = :password';
 
+
+			$statement = $db->prepare($queryUser);
+
+			$statement->bindParam(":username", $_POST["username"]);
+			$statement->bindParam(":password", $_POST["password"]);
+
+		
+			
+			$statement->execute();
+
+			$fetchAssoc = array();
+
+			$result = $statement->fetch(PDO::FETCH_ASSOC);
+
+
+		}catch(PDOException $e){
+			$messageContainer	=	'Er ging iets mis: ' . $e->getMessage();
+		}
+	}
 
 
 
 	if(isset($_POST["genereer"])){
-		$paswoord = $_SESSION["paswoord"]
+		$paswoord = $_SESSION["paswoord"];
 		generatePassword();
 	}
 
 
 	function generatePassword(){
-		return 'Banaan123'
+		return 'Banaan123';
 	}
 
 
-	$paswoord = $_SESSION['paswoord'];
+	$paswoord = $_SESSION["paswoord"];
 
 
-	if(isset($_POST['registreren'])){
-
-	}
+	
 
 ?>
